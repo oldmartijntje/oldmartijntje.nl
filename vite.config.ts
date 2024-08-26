26ce44005b528f1f05b88924d63cfcab45235730
Martijn <oldmartijntje@gmail.com>
2024-08-26 12:30:07 +0200

    test commit (gh pages)

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/',
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true, // This will clear the output directory before building
+    },
 })
